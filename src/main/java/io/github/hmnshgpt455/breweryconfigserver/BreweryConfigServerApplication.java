ebfbe07f53a90b4d9100a6dc83daecf5173c387d
hmnshgpt455 <hmnshgpt455@gmail.com>
2021-05-02 11:40:14 +0530

    Adding initial project. Closes #1.

---
+++ src/main/java/io/github/hmnshgpt455/breweryconfigserver/BreweryConfigServerApplication.java
@@ -0,0 +1,13 @@
+package io.github.hmnshgpt455.breweryconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreweryConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BreweryConfigServerApplication.class, args);
+    }
+
+}
